f0e175ff5dc19805a5f32957b335ec982469f58a
BigBlueW <badonkadooonkmc@gmail.com>
2024-10-20 23:46:22 +0800

    version 2.0
    Better UI, better used.
    More feature (while less light)

--- README.md
+++ README.md
@@ -1,44 +1,43 @@
 # Usage
 To use this program directly, you should...
 
-First, unzip the EST.zip (I had to do it, because it had exceed the 100mb limit of github) to the same directory.
+First, open the folder `readyToUseVersion`.
 
-Next, click the EST.exe. 
+Next, click the `EST.exe`. 
 
-After opened, the program allows you to press `Win+Shift+L` to make a screenshot
+After opened, this application has working in the background.
 
-(you must confirm that Win+Shift+S is working as universal windows computer.)
+Then, you are allowed to press `Win+Shift+L` to make a screenshot.
+
+(you must confirm that `Win+Shift+S` is working as universal computers under windows system.)
 
 Select the area you wanna translate. 
 
-And it will popup a notification showing the result of translator.
+And it will popup a window showing a UI of translator.
 
-(It will also copy the result into you clipboard)
+BTW, since `EST.exe` has no window shown (except the translator), 
 
-On the other hand, `Win+Shift+;` allows you to translate into English
-
-BTW, since EST.exe has no window shown, I make a close.bat that can turn off this program.
+I make a `close.bat` that can turn off this program.
 
 # Customize
 You are allowed to change
 
-1. language you wanna translate to 
+1. default language translated to
 2. key to activate the screen translate
 
-via modifying the config.txt.
+via modifying the config.txt in the folder `readyToUseVersion`.
 
 > resultLanguage = zh-TW
 > 
 > key = win+shift+l
->
-> keyEn = win+shift+;
 
-Default config is in config.txt. (worked even if config.txt is deleted)
+Default config is in `config.txt`. (worked even if `config.txt` is deleted)
 
-Language list is provided in language_list.txt.
+Language list is provided in `language_list.txt`.
 
 (Notice: Not all languages are supported.)
 
 # Others
 This program is made with __tesseract__. 
-It only supports English, Chinese, German, French, Japanese, Korean, Russian, and Spanish.
+
+It only identified characters of English, Chinese, German, French, Japanese, Korean, and Russian.
--- sourceCode/EST.py
+++ sourceCode/EST.py
@@ -1,25 +1,45 @@
-"""
-pyinstaller --onefile --add-data "Google_Translate_logo.ico;." --add-data "Tesseract-OCR;Tesseract-OCR" --hidden-import plyer.platforms.win.notification EST.py --noconsole
-"""
-import pyperclip
-from PIL import Image, ImageGrab
-import pytesseract
+#pyinstaller --noconfirm --onedir --windowed --add-data "C:\Users\Badon\OneDrive\桌面\EST\config.txt;." --add-data "C:\Users\Badon\OneDrive\桌面\EST\Google_Translate_logo.ico;." --add-data "C:\Users\Badon\OneDrive\桌面\EST\Tesseract-OCR;Tesseract-OCR/" --add-data "C:\Users\Badon\AppData\Local\Programs\Python\Python310\Lib\site-packages\customtkinter;customtkinter/"  "C:\Users\Badon\OneDrive\桌面\EST\EST.py" --noconsole
+
+import customtkinter as ctk
+import tkinter as tk
 from googletrans import Translator
 import keyboard
-import plyer
+from PIL import Image, ImageGrab
+import pytesseract
 import mouse
 import time
 import os
 import sys
+import pyperclip
+import webbrowser
 
+#setup
+extracted_text = ""
+translation_text = ""
+img = ImageGrab.grabclipboard()
+translator = Translator()
 base_dir = os.path.dirname(os.path.abspath(__file__))
-
 if getattr(sys, 'frozen', False):
     exe_path = sys.argv[0]
 else:
     exe_path = os.path.abspath(__file__)
 exe_dir = os.path.dirname(exe_path)  
-print(f"The executable is located at: {exe_dir}")
+pytesseract.pytesseract.tesseract_cmd = os.path.join(base_dir, "Tesseract-OCR", "tesseract.exe")
+
+languages = {
+    "🌍Detect language": "DL",
+    "🇺🇸English": "en",
+    "🇹🇼中文 (臺灣)": "zh-tw",
+    "🇨🇳中文 (中国)": "zh-cn",
+    "🇫🇷Français": "fr",
+    "🇩🇪Deutsch": "de",
+    "🇯🇵日本語": "ja",
+    "🇰🇷한국어": "ko",
+    "🇷🇺Русский": "ru",
+}
+
+setuped = False
+#/
 
 config = {}
 try:
@@ -33,59 +53,108 @@ except FileNotFoundError:
 #config
 resultLanguage = config.get("resultLanguage", "zh-TW")
 key = config.get("key", "win+shift+l")
-keyEn = config.get("keyEn", "win+shift+;")
-#/config
+#/
 
-def show_tooltip(msg):
-    plyer.notification.notify(
-        title="Translating result：",
-        message=msg,
-        app_icon= os.path.join(base_dir, "Google_Translate_logo.ico")
-    )
+def keyMethod():
+    global extracted_text, translation_text, img
 
-def process_image_from_clipboard(lang = resultLanguage):
+    extracted_text = ""
+    translation_text = ""
+    keyboard.press_and_release('win+shift+s')
+    while not mouse.is_pressed('left'):
+        time.sleep(0.1)
+    while mouse.is_pressed('left'):
+        time.sleep(0.1)
+    time.sleep(0.3)
     img = ImageGrab.grabclipboard()
 
     if isinstance(img, Image.Image):
-        extracted_text = pytesseract.image_to_string(img,lang="eng+chi_tra+deu+ara+fra+jpn+kor+rus+spa+chi_sim")
-        extracted_text = extracted_text.replace("\n", "")
+        extracted_text = pytesseract.image_to_string(img,lang="eng+chi_tra+fra+ara+deu+jpn+kor+rus+chi_sim")
+        extracted_text = extracted_text.replace("\n", " ")
         if extracted_text.strip():
-            translation = translator.translate(extracted_text, dest=lang)
-
-            show_tooltip(translation.text)
-            pyperclip.copy(extracted_text + "\n" + translation.text)
+            translation = translator.translate(extracted_text, dest=resultLanguage)
+            translation_text = translation.text
         else:
-            show_tooltip("No text found in the image.")
+            print("No text found in the image.")
     else:
-        show_tooltip("No image found in clipboard.")
+        print("No image found in clipboard.")
+    open_translation_window()
 
-def take_screenshot(isEng = False):
-    keyboard.press_and_release('win+shift+s')
-    print("Please do screenshot.")
-    while not mouse.is_pressed('left'):
-        time.sleep(0.1)
+def open_translation_window():
+    global extracted_text, translation_text
 
-    while mouse.is_pressed('left'):
-        time.sleep(0.1)
+    # def againButtonOnClick():
+    #     app.destroy()
+    #     keyMethod()
+    
+    def searchOnInternet():
+        def searchOnInternet2():
+            words = pyperclip.paste().replace("\n", "").replace(" ", "")
+            webbrowser.open(f"https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E/{words}")
+        keyboard.press_and_release('ctrl+c')
+        app.after(100, searchOnInternet2)
 
-    print("released")
-    time.sleep(0.3)
-    if (isEng):
-        process_image_from_clipboard("en")
-    else:
-        process_image_from_clipboard()
+    def translate_text():
+        input_text = input_box.get("1.0", tk.END)
+        src_lang = languages[src_lang_combo.get()]
+        dest_lang = languages[dest_lang_combo.get()]
+        
+        if input_text.strip():
+            if(src_lang=="DL"):
+                translated = translator.translate(input_text, dest=dest_lang)
+            else:
+                translated = translator.translate(input_text, src=src_lang, dest=dest_lang)
+            output_box.delete("1.0", tk.END)
+            output_box.insert(tk.END, translated.text)
+        else:
+            output_box.delete("1.0", tk.END)
+            output_box.insert(tk.END, "Please enter text to translate")
 
-def take_screenshot_En():
-    take_screenshot(True)
+    def check_focus():
+        if app.focus_get() is None:
+            app.destroy()
+        else:
+            app.after(500, check_focus)
 
-def bind_hotkey():
-    keyboard.add_hotkey(key, take_screenshot)
-    keyboard.add_hotkey(keyEn, take_screenshot_En)
-    print(f"{key} is set for screen translator.\n{keyEn} is set for screen translator (to English).")
-    keyboard.wait()
+    app = ctk.CTk()
+    app.title("Easy Screen Translator")
+    app.geometry("535x300")
+    app.iconbitmap(os.path.join(base_dir, "Google_Translate_logo.ico"))
+    app.resizable(False, False)
 
-if __name__ == "__main__":
-    pytesseract.pytesseract.tesseract_cmd = os.path.join(base_dir, "Tesseract-OCR", "tesseract.exe")
-    translator = Translator()
-    print(base_dir)
-    bind_hotkey()
+    app.lift()
+    app.attributes('-topmost', True)
+    app.after_idle(app.attributes, '-topmost', False)
+    app.focus_force()
+
+    app.eval('tk::PlaceWindow . center')
+
+    src_lang_combo = ctk.CTkComboBox(app, width=250, height=20, values=list(languages.keys()), state="readonly")
+    src_lang_combo.set("🌍Detect language")
+    src_lang_combo.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W+tk.E)
+
+    dest_lang_combo = ctk.CTkComboBox(app, width=250, height=20, values=[x for x in list(languages.keys()) if x not in ["Detect language"]], state="readonly")
+    dest_lang_combo.set(list(languages.keys())[list(languages.values()).index(resultLanguage.lower())])
+    dest_lang_combo.grid(row=0, column=1, padx=10, pady=10, sticky=tk.W+tk.E)
+
+    input_box = ctk.CTkTextbox(app, width=250, height=125, wrap="word")
+    input_box.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W+tk.E)
+
+    output_box = ctk.CTkTextbox(app, width=250, height=125, wrap="word")
+    output_box.grid(row=1, column=1, padx=10, pady=10, sticky=tk.W+tk.E)
+
+    difinition_button = ctk.CTkButton(app, text="Cambridge", command=searchOnInternet)
+    difinition_button.place(x=50,y=222)
+    translate_button = ctk.CTkButton(app, text="Translate", command=translate_text)
+    translate_button.place(x=350,y=222)
+
+    input_box.delete("1.0", tk.END)
+    input_box.insert(tk.END, extracted_text)
+    output_box.delete("1.0", tk.END)
+    output_box.insert(tk.END, translation_text)
+
+    app.after(500, check_focus)
+
+    app.mainloop()
+keyboard.add_hotkey(key, keyMethod)
+keyboard.wait()
